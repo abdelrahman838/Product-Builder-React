9258ee3064b6739bd4173e98f3790ee4d18f7449
Abdelrahman Abdelhamid <abdelrahmanhamid99@gmail.com>
2025-09-07 04:40:57 +0300

    Card tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,18 @@
 import './App.css'
+import ProductCard from './components/ProductCard'
 
 function App() {
 
   return (
     <>
-    <div>
-  <label className="block text-sm/6 font-medium text-white">Price</label>
-  <div className="mt-2">
-    <div className="flex items-center rounded-md bg-white/5 pl-3 outline-1 -outline-offset-1 outline-gray-600 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-500">
-      <div className="shrink-0 text-base text-gray-400 select-none sm:text-sm/6">$</div>
-      <input id="price" type="text" name="price" placeholder="0.00" className="block min-w-0 grow bg-gray-800 py-1.5 pr-3 pl-1 text-base text-white placeholder:text-gray-500 focus:outline-none sm:text-sm/6" />
-      <div className="grid shrink-0 grid-cols-1 focus-within:relative">
-        <select id="currency" name="currency" aria-label="Currency" className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-gray-800 py-1.5 pr-7 pl-3 text-base text-gray-400 *:bg-gray-800 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6">
-          <option>USD</option>
-          <option>CAD</option>
-          <option>EUR</option>
-        </select>
-        <svg viewBox="0 0 16 16" fill="currentColor" data-slot="icon" aria-hidden="true" className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-400 sm:size-4">
-          <path d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" fill-rule="evenodd" />
-        </svg>
-      </div>
+    <div className="bg-indigo-500 m-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 p-2 rounded-2xl">
+    <ProductCard></ProductCard>
+    <ProductCard></ProductCard>
+    <ProductCard></ProductCard>
+    <ProductCard></ProductCard>
+    <ProductCard></ProductCard>
+    <ProductCard></ProductCard>
     </div>
-  </div>
-</div>
     </>
   )
 }
---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,12 @@
+
+
+
+const ProductCard = () =>{
+  return(
+    <div className="border border-white text-white rounded-md text-sm p-3">
+      <span>Product Card</span>
+    </div>
+  )
+}
+
+export default ProductCard
