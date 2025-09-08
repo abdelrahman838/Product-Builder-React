4bde6b207c324a49f3cb485dc5fb2c97ccf6c4da
Abdelrahman Abdelhamid <abdelrahmanhamid99@gmail.com>
2025-09-08 08:34:25 +0300

    Made intial product cards UI

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-    <div className="bg-indigo-500 m-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 p-2 rounded-2xl">
+    <div className="m-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 p-2 rounded-2xl">
     <ProductCard></ProductCard>
     <ProductCard></ProductCard>
     <ProductCard></ProductCard>
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -3,8 +3,27 @@
 
 const ProductCard = () =>{
   return(
-    <div className="border border-white text-white rounded-md text-sm p-3">
-      <span>Product Card</span>
+    <div className="border border-black text-black rounded-md p-3 flex flex-col">
+      <img src="https://cdn.pixabay.com/photo/2025/08/11/07/18/nurturing-swan-9767495_1280.jpg" alt="product photo" />
+
+      <h3>Product Card</h3>
+      <p>Lorem, ipsum dolor.</p>
+
+      <div className="flex items-center my-4 space-x-2">
+        <span className="w-5 h-5 bg-indigo-600 rounded-full"></span>
+        <span className="w-5 h-5 bg-yellow-600 rounded-full"></span>
+        <span className="w-5 h-5 bg-red-600 rounded-full"></span>
+      </div>
+
+      <div className="flex items-center justify-between">
+        <span>500,000</span>
+        <img className="w-10 h-10 rounded-full" src="https://cdn.pixabay.com/photo/2025/08/11/07/18/nurturing-swan-9767495_1280.jpg" alt="product photo" />
+      </div>
+
+      <div className="flex items-center justify-between space-x-2 my-5">
+        <button className="bg-indigo-700 p-2 w-full rounded-md cursor-pointer">EDIT</button>
+        <button className="bg-red-700 p-2 w-full rounded-md cursor-pointer">DELETE</button>
+      </div>
     </div>
   )
 }
