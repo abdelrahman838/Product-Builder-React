ff351c1f340262ecb3a81b8af656dbca5df2d8ca
Abdelrahman Abdelhamid <abdelrahmanhamid99@gmail.com>
2025-10-05 03:22:51 +0300

    adjusting container

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,11 @@ function App() {
 
   return (
     <>
+    <main className='container mx-auto'>
       <div className="m-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 p-2 rounded-2xl">
         {renderProductList}
       </div>
+    </main>
     </>
   )
 }
